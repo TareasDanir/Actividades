d4209174c1be2aa8c0e6c7eb0b95ce9b7c4c675f
Danir1509 <2524572019@mail.utec.edu.sv>
2020-11-14 22:13:26 -0600

    Introduccion a Mvc

---
+++ IntroduccionMVC/IntroduccionMVC/Controllers/OperacionesController.cs
@@ -0,0 +1,81 @@
+﻿using IntroduccionMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionMVC.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // GET: Operaciones
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+            Datos objModelo = new Datos();
+            objModelo.Variable_A = 3;
+            objModelo.Variable_B = 5;
+            objModelo.Resultado = objModelo.Variable_A + objModelo.Variable_B;
+            ViewData["Resultado"] = objModelo.Resultado;
+            return View();
+        }
+
+        public ActionResult Resta()
+        {
+            Datos objModelo = new Datos();
+            objModelo.Variable_A = double.Parse(Request.Form["valor1"].ToString());
+            objModelo.Variable_B = double.Parse(Request.Form["valor2"].ToString()); ;
+            objModelo.Resultado = objModelo.Variable_A - objModelo.Variable_B;
+            return View("Resta",objModelo);
+        }
+
+        public ActionResult Fal()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Fal( Datos objModelo)
+        {
+            
+            objModelo.Variable_A = double.Parse(Request.Form["a"].ToString());
+            objModelo.Variable_B = double.Parse(Request.Form["b"].ToString()); ;
+            int opcion = objModelo.opcion;
+            double resultado;
+
+            switch (opcion)
+            {
+                case 1:
+                    //Multiplicar
+                    objModelo.Resultado = objModelo.Variable_A * objModelo.Variable_B;
+                    ViewBag.Resultado = objModelo.Resultado;
+                    break;
+                case 2:
+                    //Dividir
+                    if (objModelo.Variable_B==0)
+                    {
+                        ViewBag.Resultado = "No se puede dividir entre cero";
+                    }
+                    else {
+                        objModelo.Resultado = objModelo.Variable_A / objModelo.Variable_B;
+                        ViewBag.Resultado = objModelo.Resultado;
+                    }
+                    
+                    break;
+                case 0:
+                    //Sin opcion
+                    //resta
+                    ViewBag.Resultado = "No a seleccionado ninguna accion";
+                    break;
+            }
+            
+            return View();
+        }
+
+    }
+}
---
+++ IntroduccionMVC/IntroduccionMVC/Models/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionMVC.Models
+{
+    public class Datos
+    {
+        public double Variable_A { get; set; }
+        public double Variable_B { get; set; }
+        public double Resultado { get; set; }
+        public int opcion { get; set; }
+    }
+}
